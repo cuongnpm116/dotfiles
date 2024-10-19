c89dcd98c2e36fcd1890e6d271664b38ae5a8ec3
Cuong Nguyen <cuongnpm116@gmail.com>
2024-10-19 17:19:08 +0700

    add tokyonight colorscheme for powershell

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,8 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 
 local opts = { noremap = true, silent = true }
 -- delete single character without yanking into register
@@ -21,9 +19,3 @@ vim.keymap.set("v", "p", '"_dP', opts)
 
 -- J but keep cursor at beginning position
 vim.keymap.set("n", "J", "mzJ`z")
-
--- yank to system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-
--- delete but not save to clipboard
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
