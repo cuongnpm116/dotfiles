edc010be2ea9b614c6a43424a85f503f661c7232
Cuong Nguyen <cuongnpm116@gmail.com>
2025-08-08 18:11:42 +0700

    moving to hyprland

--- .bashrc
+++ .bashrc
@@ -116,8 +116,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-. "$HOME/.cargo/env"
-
 export PATH=$PATH:/usr/local/go/bin
 
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,13 +1,27 @@
 local wezterm = require('wezterm') --[[@as Wezterm]]
-local background = require('background')
 local config = {}
 
-config.font = wezterm.font('JetBrainsMono Nerd Font Mono')
+config.enable_wayland = true
+
+config.font = wezterm.font('Hack Nerd Font Mono')
 config.font_size = 14
 
 -- config.window_decorations = 'RESIZE'
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
+
+local home = os.getenv('HOME')
+
+local background = {
+    {
+        source = {
+            File = home .. '/Pictures/anime-girl-white-hair.jpg',
+        },
+        hsb = {
+            brightness = 0.01,
+        },
+    },
+}
 
 config.background = background
 config.color_scheme = 'Tokyo Night'
