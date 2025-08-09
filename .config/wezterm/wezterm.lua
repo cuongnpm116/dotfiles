e6e037d693de4c240cf55668fe0322bf21d750ae
Cuong Nguyen <cuongnpm116@gmail.com>
2025-08-09 23:12:10 +0700

    move back to i3

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,8 @@
 local wezterm = require('wezterm') --[[@as Wezterm]]
 local config = {}
 
-config.enable_wayland = true
+-- config.enable_wayland = true
+config.use_ime = true
 
 config.font = wezterm.font('Hack Nerd Font Mono')
 config.font_size = 14
@@ -15,15 +16,22 @@ local home = os.getenv('HOME')
 local background = {
     {
         source = {
-            File = home .. '/Pictures/anime-girl-white-hair.jpg',
+            File = home .. '/Pictures/Wallpapers/dead-by-daylight-game-maniac.jpg',
         },
         hsb = {
             brightness = 0.01,
         },
     },
 }
-
 config.background = background
+
 config.color_scheme = 'Tokyo Night'
+
+-- config.window_background_opacity = 0.9
+config.colors = {
+    tab_bar = {
+        background = 'rgba(0,0,0,0)',
+    },
+}
 
 return config
--- readme.md
+++ readme.md
@@ -30,3 +30,4 @@ then use Stow to create symlinks
 ```
 stow .
 ```
+
