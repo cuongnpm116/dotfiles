22ebd449e092e631093f40b3f484d2ac4b342e18
Cuong Nguyen <cuongnpm116@gmail.com>
2025-12-19 16:50:24 +0700

    back after a while

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,5 +1,4 @@
 return {
-  -- default version = false. no need to declare
   { 'nvim-mini/mini.icons', opts = {} },
   { 'nvim-mini/mini.pairs', opts = {} },
   { 'nvim-mini/mini.surround', opts = {} },
